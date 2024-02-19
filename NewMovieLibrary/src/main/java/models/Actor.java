4dd9dcf45f7b859eb7c0fa0eaad3fca782efea88
wkomorowski <wkomorowski@sii.pl>
2024-02-19 10:48:34 +0100

    update Actor and Director classes

--- NewMovieLibrary/src/main/java/models/Actor.java
+++ NewMovieLibrary/src/main/java/models/Actor.java
@@ -1,5 +1,35 @@
 package models;
 
 public class Actor {
+    private String firstName;
+    private String lastName;
 
+    public Actor(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Actor{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
