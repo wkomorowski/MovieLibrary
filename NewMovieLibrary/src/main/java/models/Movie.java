c5f2b352c58df7d312684f1f11ad6fbe2dce34a4
wkomorowski <wkomorowski@sii.pl>
2024-02-19 12:09:04 +0100

    update Movie class

--- NewMovieLibrary/src/main/java/models/Movie.java
+++ NewMovieLibrary/src/main/java/models/Movie.java
@@ -1,4 +1,59 @@
 package models;
 
+import javax.swing.*;
+import java.util.List;
+
 public class Movie {
+    private String title;
+    private Director director;
+    private List<Actor> listOfActors;
+
+    public Movie(String title, Director director, List<Actor> listOfActors) {
+        this.title = title;
+        this.director = director;
+        this.listOfActors = listOfActors;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Director getDirector() {
+        return director;
+    }
+
+    public void setDirector(Director director) {
+        this.director = director;
+    }
+
+    public List<Actor> getListOfActors() {
+        return listOfActors;
+    }
+
+    public void setListOfActors(List<Actor> listOfActors) {
+        this.listOfActors = listOfActors;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "title='" + title + '\'' +
+                ", director=" + director +
+                ", listOfActors=" + listOfActors +
+                '}';
+    }
+    private String printActorsInARow() {
+        var actorInARow = new StringBuilder();
+        String separator = "";
+        for (Actor listOfActor : listOfActors) {
+            actorInARow.append(separator + listOfActor + "");
+            separator = ", ";
+        }
+        return actorInARow.toString();
+    }
+
 }
